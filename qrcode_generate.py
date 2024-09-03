e8c4fb01ee9eb12d23f953c066a58789876f4298
Chandra Shekhar Kumar <kchandrashekhar1603@gmail.com>
2024-09-03 16:44:53 +0530

    Second commit

--- qrcode_generate.py
+++ qrcode_generate.py
@@ -1,4 +1,14 @@
+#QR code generate
 import qrcode
-myqr = qrcode.make("https://www.instagram.com/chandra_._shekhar/")
+from pyzbar.pyzbar import decode
+from PIL import Image
 
-myqr.save("myqr.png")
+myqr = qrcode.make("https://www.instagram.com/chandra_._shekhar/")
+myqr1=qrcode.make("https://www.linkedin.com/in/chandra-shekhar-k/")
+
+myqr.save("myqr.png", scale=8)
+myqr1.save("myqr1.png", scale=8)
+
+#decode QR Code
+b=decode(Image.open("myqr.png"))
+print(b[0].data.decode("ascii"))
